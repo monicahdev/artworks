c847ff185c9d3e900ca30590b9e3aefd1685dd3d
Monica Hernandez <monicahdev@gmail.com>
2025-12-06 14:39:02 +0100

    Created inital routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ArtDetailComponent } from './pages/art-detail/art-detail.component';
+import { ArtListComponent } from './pages/art-list/art-list.component';
+export const routes: Routes = [
+  { path: '', redirectTo: 'photos', pathMatch: 'full' },
+  { path: 'art', component: ArtListComponent },
+  { path: 'art/:id', component: ArtDetailComponent },
+];
