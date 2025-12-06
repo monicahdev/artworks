f1a38fba5865231d170a729fbe6d8636cdc6449c
Monica Hernandez <monicahdev@gmail.com>
2025-12-06 14:36:58 +0100

    Created art list and art detail components

---
+++ src/app/pages/art-detail/art-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-art-detail',
+  imports: [],
+  templateUrl: './art-detail.component.html',
+  styleUrl: './art-detail.component.scss'
+})
+export class ArtDetailComponent {
+
+}
---
+++ src/app/pages/art-list/art-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-art-list',
+  imports: [],
+  templateUrl: './art-list.component.html',
+  styleUrl: './art-list.component.scss'
+})
+export class ArtListComponent {
+
+}
