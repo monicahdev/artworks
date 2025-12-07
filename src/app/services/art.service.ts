5effe348d5180e4c756a9942e0b93e07aa680f23
Monica Hernandez <monicahdev@gmail.com>
2025-12-07 15:36:06 +0100

    Created getArt method
    Api does not return array. It returns objects. Interface for ArtApiResponse was created to complete de get request as the example

--- src/app/services/art.service.ts
+++ src/app/services/art.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Artwork } from '../models/art.interface';
 
+//la api no devuelve un array, como en el ejemplo
+interface ArtApiResponse {
+  data: Artwork[];
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArtService {
+  private api = 'https://api.artic.edu/api/v1/artworks?page=2&limit=25';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getArt(): Observable<Artwork[]> {
+    return this.http.get<ArtApiResponse>(this.api).pipe(map((res) => res.data));
+  }
 }
