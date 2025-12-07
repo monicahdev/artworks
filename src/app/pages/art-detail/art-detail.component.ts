34e8666670a87353f84e1c20824b6dc12ba8773d
Monica Hernandez <monicahdev@gmail.com>
2025-12-07 19:19:53 +0100

    Added signal based on interface and ngOnInit method based on PEC4Teoria example

--- src/app/pages/art-detail/art-detail.component.ts
+++ src/app/pages/art-detail/art-detail.component.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Artwork } from '../../models/art.interface';
+import { ArtService } from '../../services/art.service';
 @Component({
   selector: 'app-art-detail',
   imports: [],
   templateUrl: './art-detail.component.html',
   styleUrl: './art-detail.component.scss',
 })
-export class ArtDetailComponent {}
+export class ArtDetailComponent {
+  artworkDetail = signal<Artwork>({
+    id: 0,
+    title: '',
+    creation_date: '',
+    technique: '',
+    measurements: '',
+    creators: [],
+    images: {
+      web: {
+        url: '',
+        width: 0,
+        height: 0,
+      },
+    },
+  });
+
+  constructor(
+    private artService: ArtService,
+    //para leer parámetros de la url
+    private activatedRoute: ActivatedRoute,
+    //para redireccionar a una vista determinana si no tenemos un identificador válido
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const identifier = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log('Identifier --->', identifier);
+
+    if (identifier) {
+      this.artService.getArtworkById(identifier).subscribe((artwork) => {
+        if (!artwork) {
+          this.router.navigateByUrl('/');
+        }
+
+        this.artworkDetail.set(artwork);
+        console.log('artworkDetail -->', this.artworkDetail());
+      });
+    }
+  }
+}
