6c04526afe867c5de3e5477ade5280e73e887b13
Monica Hernandez <monicahdev@gmail.com>
2025-12-07 15:51:38 +0100

    Corrected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { ArtDetailComponent } from './pages/art-detail/art-detail.component';
 import { ArtListComponent } from './pages/art-list/art-list.component';
 export const routes: Routes = [
-  { path: '', redirectTo: 'photos', pathMatch: 'full' },
-  { path: 'art', component: ArtListComponent },
-  { path: 'art/:id', component: ArtDetailComponent },
+  { path: '', redirectTo: 'artworks', pathMatch: 'full' },
+  { path: 'artworks', component: ArtListComponent },
+  { path: 'artworks/:id', component: ArtDetailComponent },
 ];
