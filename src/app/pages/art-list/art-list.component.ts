9a5af076a24aa761ad70947d52168e8e6c42a8c2
Monica Hernandez <monicahdev@gmail.com>
2025-12-07 15:38:13 +0100

    Created ngOnInit method to get artworks results from api

--- src/app/pages/art-list/art-list.component.ts
+++ src/app/pages/art-list/art-list.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Artwork } from '../../models/art.interface';
+import { ArtService } from '../../services/art.service';
 @Component({
   selector: 'app-art-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './art-list.component.html',
-  styleUrl: './art-list.component.scss'
+  styleUrl: './art-list.component.scss',
 })
 export class ArtListComponent {
+  artworks = signal<Artwork[]>([]);
 
+  constructor(private artService: ArtService) {}
+
+  ngOnInit(): void {
+    this.artService
+      .getArt()
+      .subscribe((artworks) => this.artworks.set(artworks));
+  }
 }
