fa97a78719b0e8c9b3b612b0bc662625f0ce6504
Monica Hernandez <monicahdev@gmail.com>
2025-12-14 23:43:02 +0100

    Created artwork grid component ts
    on clic, redirects to artwork detail

--- src/app/components/artwork-grid/artwork-grid.component.ts
+++ src/app/components/artwork-grid/artwork-grid.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { Artwork } from '../../models/art.interface';
 
 @Component({
   selector: 'app-artwork-grid',
-  imports: [],
+  imports: [MatTableModule, MatButtonModule],
   templateUrl: './artwork-grid.component.html',
-  styleUrl: './artwork-grid.component.scss'
+  styleUrl: './artwork-grid.component.scss',
 })
 export class ArtworkGridComponent {
+  @Input({ required: true }) artworks: Artwork[] = [];
 
+  constructor(private router: Router) {}
+
+  goToArtworkDetail(row: Artwork) {
+    this.router.navigate(['/artworks', row.id]);
+  }
 }
