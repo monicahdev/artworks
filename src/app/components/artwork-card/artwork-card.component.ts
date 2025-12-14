0df1fecc9a66f3c6729fa611f249176bd1b25eaa
Monica Hernandez <monicahdev@gmail.com>
2025-12-14 23:39:22 +0100

    Created artwork card component ts
    on clic, redirects to artwork detail

--- src/app/components/artwork-card/artwork-card.component.ts
+++ src/app/components/artwork-card/artwork-card.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { Artwork } from '../../models/art.interface';
 
 @Component({
   selector: 'app-artwork-card',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './artwork-card.component.html',
-  styleUrl: './artwork-card.component.scss'
+  styleUrl: './artwork-card.component.scss',
 })
 export class ArtworkCardComponent {
+  @Input({ required: true }) artwork!: Artwork;
 
+  constructor(private router: Router) {}
+
+  goToArtworkDetail() {
+    this.router.navigate(['/artworks', this.artwork.id]);
+  }
 }
