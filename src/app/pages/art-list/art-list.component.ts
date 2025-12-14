06ea9885a0396c6479aa7df9c77efca4531acf32
Monica Hernandez <monicahdev@gmail.com>
2025-12-15 00:25:10 +0100

    Imported components and created cards/table view methods

--- src/app/pages/art-list/art-list.component.ts
+++ src/app/pages/art-list/art-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ArtworkCardComponent } from '../../components/artwork-card/artwork-card.component';
+import { ArtworkGridComponent } from '../../components/artwork-grid/artwork-grid.component';
 import { Artwork } from '../../models/art.interface';
 import { ArtService } from '../../services/art.service';
 @Component({
   selector: 'app-art-list',
-  imports: [RouterLink],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    ArtworkCardComponent,
+    ArtworkGridComponent,
+  ],
   templateUrl: './art-list.component.html',
   styleUrl: './art-list.component.scss',
 })
 export class ArtListComponent {
   artworks = signal<Artwork[]>([]);
+
+  viewMode = signal<'cards' | 'table'>('cards');
 
   constructor(private artService: ArtService) {}
 
@@ -17,5 +27,12 @@ export class ArtListComponent {
     this.artService
       .getArtworks()
       .subscribe((artworks) => this.artworks.set(artworks));
+  }
+
+  setCardsView() {
+    this.viewMode.set('cards');
+  }
+  setTableView() {
+    this.viewMode.set('table');
   }
 }
