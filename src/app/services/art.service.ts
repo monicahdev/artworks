9079ad09f3916bc9ad3e93c4bfdb1e5e466153de
Monica Hernandez <monicahdev@gmail.com>
2025-12-14 19:40:15 +0100

    Corrected api url

--- src/app/services/art.service.ts
+++ src/app/services/art.service.ts
@@ -1,25 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 import { Artwork } from '../models/art.interface';
 @Injectable({
   providedIn: 'root',
 })
 export class ArtService {
-  private api = environment.apiUrl;
+  private api = 'https://openaccess-api.clevelandart.org/api/artworks/';
 
   constructor(private http: HttpClient) {}
 
   getArtworks(): Observable<Artwork[]> {
     return this.http
-      .get<{ data: Artwork[] }>(this.api + '/artworks?limit=50')
+      .get<{ data: Artwork[] }>(this.api + '?limit=50')
       .pipe(map((res) => res.data));
   }
 
   getArtworkById(id: number | string): Observable<Artwork> {
     return this.http
-      .get<{ data: Artwork }>(this.api + '/artworks/' + id)
+      .get<{ data: Artwork }>(this.api + id)
       .pipe(map((res) => res.data));
   }
 }
