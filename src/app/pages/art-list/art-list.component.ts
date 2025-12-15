a7ab728c5e43f8e55eb7f245dabe979b17395d4a
Monica Hernandez <monicahdev@gmail.com>
2025-12-15 12:26:08 +0100

    Added spinner while loading logic

--- src/app/pages/art-list/art-list.component.ts
+++ src/app/pages/art-list/art-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ArtworkCardComponent } from '../../components/artwork-card/artwork-card.component';
 import { ArtworkGridComponent } from '../../components/artwork-grid/artwork-grid.component';
+
 import { Artwork } from '../../models/art.interface';
 import { ArtService } from '../../services/art.service';
 @Component({
@@ -12,21 +14,27 @@ import { ArtService } from '../../services/art.service';
     MatIconModule,
     ArtworkCardComponent,
     ArtworkGridComponent,
+    MatProgressSpinnerModule,
   ],
   templateUrl: './art-list.component.html',
   styleUrl: './art-list.component.scss',
 })
 export class ArtListComponent {
   artworks = signal<Artwork[]>([]);
-
   viewMode = signal<'cards' | 'table'>('cards');
+  isLoading = signal<boolean>(true);
 
   constructor(private artService: ArtService) {}
 
   ngOnInit(): void {
-    this.artService
-      .getArtworks()
-      .subscribe((artworks) => this.artworks.set(artworks));
+    this.isLoading.set(true);
+    this.artService.getArtworks().subscribe({
+      next: (artworks) => {
+        this.artworks.set(artworks);
+        this.isLoading.set(false);
+      },
+      error: () => this.isLoading.set(false),
+    });
   }
 
   setCardsView() {
